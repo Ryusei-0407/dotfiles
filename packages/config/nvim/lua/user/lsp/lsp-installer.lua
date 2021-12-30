1322df89bbc4a142e863e248d492a962dcffa8c6
Ryusei-0407 <ryu000407@gmail.com>
2021-12-31 02:06:13 +0900

    add grraphql lsp

--- packages/config/nvim/lua/user/lsp/lsp-installer.lua
+++ packages/config/nvim/lua/user/lsp/lsp-installer.lua
@@ -63,6 +63,11 @@ lsp_installer.on_server_ready(function(server)
         opts = vim.tbl_deep_extend('force', eslint_opts, opts)
     end
 
+    if server.name == 'graphql' then
+        local graphql_opts = require('user.lsp.settings.graphql')
+        opts = vim.tbl_deep_extend('force', graphql_opts, opts)
+    end
+
     if server.name == 'solidity_ls' then
         local solidity_ls_opts = require('user.lsp.settings.solidity_ls')
         opts = vim.tbl_deep_extend('force', solidity_ls_opts, opts)
