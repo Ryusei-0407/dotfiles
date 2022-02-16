c9562d6d24a85b45c7838a75da258cc753d3f185
ryu <ryu000407@gmail.com>
2022-02-16 13:30:37 +0900

    delete rls

--- packages/config/nvim/lua/user/lsp/init.lua
+++ packages/config/nvim/lua/user/lsp/init.lua
@@ -6,4 +6,5 @@ end
 require("user.lsp.lsp-installer")
 require("user.lsp.handlers").setup()
 require("rust-tools").setup()
+require("rust-tools.inlay_hints").set_inlay_hints()
 require("user.lsp.null-ls")
