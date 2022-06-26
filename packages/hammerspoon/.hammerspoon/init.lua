a5bce0c2e7c4845d2433f98b3e87d88b42627a1c
ryu <ryu000407@gmail.com>
2022-06-26 17:52:23 +0900

    update: change kitty to alactitty

--- packages/hammerspoon/.hammerspoon/init.lua
+++ packages/hammerspoon/.hammerspoon/init.lua
@@ -1,21 +1,10 @@
-hs.hotkey.bind({'ctrl'}, 'g', function()
-    local kitty = hs.application.get('kitty')
-    if kitty == nil then
-        hs.application.launchOrFocus('/Applications/kitty.app')
-    elseif kitty:isFrontmost() then
-        kitty:hide()
-    else
-        hs.application.launchOrFocus('/Applications/kitty.app')
-    end
-end)
-
-hs.hotkey.bind({'ctrl', 'shift'}, 'b', function()
-    local brave = hs.application.get('Brave Browser')
-    if brave == nil then
-        hs.application.launchOrFocus('/Applications/Brave Browser.app')
-    elseif brave:isFrontmost() then
-        brave:hide()
-    else
-        hs.application.launchOrFocus('/Applications/Brave Browser.app')
-    end
+hs.hotkey.bind({ "ctrl" }, "g", function()
+	local alacritty = hs.application.get("Alacritty")
+	if alacritty == nil then
+		hs.application.launchOrFocus("/Applications/Alacritty.app")
+	elseif alacritty:isFrontmost() then
+		alacritty:hide()
+	else
+		hs.application.launchOrFocus("/Applications/Alacritty.app")
+	end
 end)
