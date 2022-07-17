ac74aaa5222c882b26e64fe7a7c61cc224da5f11
ryu <ryu000407@gmail.com>
2022-07-17 23:30:47 +0900

    update: install script

--- install.sh
+++ install.sh
@@ -10,25 +10,29 @@ if ! [ $ARCH = arm64 ]; then
 fi
 
 if ! [ -x "$(command -v brew)" ]; then
+    echo >&2 "Install 'brew'"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 if ! [ -x "$(command -v git)" ]; then
+    echo >&2 "Install 'git'"
     brew install git
 fi
 
 if ! [ -d $HOME/dotfiles ]; then
+    echo >&2 "Download dotfiles"
     cd && git clone git@github.com:Ryusei-0407/dotfiles.git
 fi
 
+echo >&2 "Install any packages"
 brew bundle -v --file=~/dotfiles/Brewfile
 
 if ! [ -d $HOME/.config ]; then
     mkdir $HOME/.config
 fi
 
-# Rust
 if ! [ -x "$(command -v rustup)" ]; then
+    echo >&2 "Install rustup"
     curl https://sh.rustup.rs -sSf | sh
 fi
 
