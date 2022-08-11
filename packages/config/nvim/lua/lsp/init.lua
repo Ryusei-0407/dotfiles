20778e1243e4587d4013aa076411a4e6360638a5
ryu <ryu000407@gmail.com>
2022-08-12 02:10:43 +0900

    style(lsp): change oneline

--- packages/config/nvim/lua/lsp/init.lua
+++ packages/config/nvim/lua/lsp/init.lua
@@ -1,13 +1,9 @@
 local status_ok, mason = pcall(require, "mason")
-if not status_ok then
-    return
-end
+if not status_ok then return end
 mason.setup()
 
 local status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
-if not status_ok then
-    return
-end
+if not status_ok then return end
 mason_lspconfig.setup_handlers({ function(server)
     local opt = {
         capabilities = require("cmp_nvim_lsp").update_capabilities(
