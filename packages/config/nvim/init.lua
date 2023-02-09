714baebaf11f0b730c99fe23471bd5fedc936f25
ryu <ryu000407@gmail.com>
2023-02-10 03:12:22 +0900

    update(nvim): change packer to lazy

--- packages/config/nvim/init.lua
+++ packages/config/nvim/init.lua
@@ -31,9 +31,8 @@ vim.opt.wrap = false
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 vim.opt.guifont = "monospace:h10"
-
-vim.cmd([[autocmd! FileType typescript,typescriptreact,terraform setlocal shiftwidth=2 softtabstop=2]])
-
+vim.opt.ambiwidth = "single"
+vim.opt.undodir = vim.fn.stdpath("cache") .. "/undo"
 vim.opt.shortmess:append("c")
 
 vim.cmd("set whichwrap+=<,>,h,l")
@@ -59,19 +58,6 @@ vim.g.loaded_tutor_mode_plugin = 1
 vim.g.loaded_zipPlugin = 1
 vim.g.skip_loading_mswin = 1
 
-vim.g.dracula_colortem = 0
-vim.cmd([[
-    try
-        colorscheme dracula
-        colorscheme dracula_pro
-    catch /^Vim\%((\a\+)\)\=:E185/
-        colorscheme default
-    endtry
-
-]])
-
-vim.cmd([[autocmd! BufRead,BufEnter *.astro set filetype=astro]])
-
 local opts = { noremap = true, silent = true }
 
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", opts)
@@ -87,18 +73,14 @@ vim.api.nvim_set_keymap("n", "ss", ":split<CR>", opts)
 vim.api.nvim_set_keymap("n", "sv", ":vsplit<CR>", opts)
 
 -- lazygit
-vim.api.nvim_set_keymap("n", "gl", ":LazyGit<CR>", opts)
-
+vim.api.nvim_set_keymap("n", "lg", ":LazyGit<CR>", opts)
 -- neo-tree
 vim.api.nvim_set_keymap("n", "<C-e>", ":Neotree toggle=true<CR>", opts)
-
 -- telescope
 vim.api.nvim_set_keymap("n", "<leader>f", "<cmd>Telescope find_files<CR>", opts)
 vim.api.nvim_set_keymap("n", "<C-t>", "<cmd>Telescope live_grep<CR>", opts)
-
 -- winresizer
 vim.g.winresizer_start_key = "<C-s>"
-
 -- barbar
 vim.api.nvim_set_keymap("n", "<Space>h", ":BufferPrevious<CR>", opts)
 vim.api.nvim_set_keymap("n", "<Space>l", ":BufferNext<CR>", opts)
@@ -106,87 +88,188 @@ vim.api.nvim_set_keymap("n", "<Space>,", ":BufferMovePrevious<CR>", opts)
 vim.api.nvim_set_keymap("n", "<Space>.", ":BufferMoveNext<CR>", opts)
 vim.api.nvim_set_keymap("n", "<Space>w", ":BufferClose<CR>", opts)
 
-require("impatient")
-require("config.lualine")
-require("config.alpha")
-require("config.telescope")
-require("config.cmp")
-require("gitsigns").setup()
-require("lsp")
-require("rust-tools").setup()
-require("nvim-ts-autotag").setup()
-require("fidget").setup()
-require("trouble").setup()
-require("toggleterm").setup({
-	open_mapping = [[<C-\>]],
-	size = 20,
-	hide_numbers = true,
-	direction = "float",
-	close_on_exit = true,
-})
-require("nvim-treesitter.configs").setup({
-	ensure_installed = "all",
-	ignore_install = { "phpdoc" },
-	context_commentstring = {
-		enable = true,
-	},
-	highlight = {
-		enable = true,
-		disable = { "lua" },
-	},
-	indent = {
-		enable = true,
-	},
-    rainbow = {
-        enable = true,
-        extended_mode = true,
-        max_file_lines = nil,
-    },
-})
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
 
-return require("packer").startup(function()
-	use("wbthomason/packer.nvim")
-	use("nvim-lualine/lualine.nvim")
-	use("nvim-lua/popup.nvim")
-	use("nvim-treesitter/nvim-treesitter")
-    use("fedepujol/bracket-guides")
-	use("goolord/alpha-nvim")
-	use("nvim-lua/plenary.nvim")
-	use("kyazdani42/nvim-web-devicons")
-	use({
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v2.x",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"kyazdani42/nvim-web-devicons",
-			"MunifTanjim/nui.nvim",
-		},
-	})
-    use({"folke/trouble.nvim", requires = { "kyazdani42/nvim-web-devicons" } })
-	use("nvim-telescope/telescope.nvim")
-	use("nvim-telescope/telescope-media-files.nvim")
-	use({ "yioneko/nvim-yati", requires = { "nvim-treesitter/nvim-treesitter" } })
-	use({ "romgrk/barbar.nvim", requires = { "kyazdani42/nvim-web-devicons" } })
-	use("Mofiqul/dracula.nvim")
-	use("machakann/vim-sandwich")
-	use("simeji/winresizer")
-	use({ "akinsho/toggleterm.nvim", tag = "v2.*" })
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-cmdline")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-nvim-lua")
-	use({ "L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*" })
-	use("lewis6991/gitsigns.nvim")
-    use("kdheepak/lazygit.nvim")
-	use("neovim/nvim-lspconfig")
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
-	use("jose-elias-alvarez/null-ls.nvim")
-	use("MunifTanjim/prettier.nvim")
-	use("simrat39/rust-tools.nvim")
-	use("windwp/nvim-ts-autotag")
-    use("j-hui/fidget.nvim")
-	use("lewis6991/impatient.nvim")
-end)
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+    {
+        "nvim-lualine/lualine.nvim",
+        lazy = false,
+        config = true,
+    },
+    {
+        "nvim-lua/popup.nvim",
+        lazy = true,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        config = function()
+            require("nvim-treesitter/configs").setup({
+                ensure_installed = "all",
+                ignore_install = { "phpdoc" },
+                context_commentstring = {
+                    enable = true,
+                },
+                highlight = {
+                    enable = true,
+                    disable = { "lua" },
+                },
+                indent = {
+                    enable = true,
+                },
+                rainbow = {
+                    enable = true,
+                    extended_mode = true,
+                    max_file_lines = nil,
+                },
+            })
+        end
+    },
+    {
+        "fedepujol/bracket-guides",
+        lazy = true,
+    },
+    {
+        "goolord/alpha-nvim",
+        config = function()
+            require("config/alpha")
+        end
+    },
+    {
+        "nvim-lua/plenary.nvim",
+        lazy = true,
+    },
+    "kyazdani42/nvim-web-devicons",
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        lazy = true,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "nvim-lua/plenary.nvim",
+            "kyazdani42/nvim-web-devicons",
+        },
+        keys = {
+            { "<C-e>", "<CMD>Neotree toggle=true<CR>" }
+        },
+        config = function()
+            require("config/neo-tree")
+        end
+    },
+    {
+        "folke/trouble.nvim",
+        lazy = true,
+        dependencies = { "kyazdani42/nvim-web-devicons" },
+        config = true,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        lazy = true,
+        dependencies = { "nvim-telescope/telescope-media-files.nvim" },
+        config = function()
+            require("config/telescope")
+        end
+    },
+    {
+        "yioneko/nvim-yati",
+        lazy = false,
+        dependencies = { "nvim-treesitter/nvim-treesitter" },
+    },
+    {
+        "romgrk/barbar.nvim",
+        lazy = false,
+        dependencies = { "kyazdani42/nvim-web-devicons" },
+    },
+    {
+        "Mofiqul/dracula.nvim",
+        lazy = false,
+        config = function()
+            vim.cmd([[
+                colorscheme dracula
+                colorscheme dracula_pro
+            ]])
+        end
+    },
+    "machakann/vim-sandwich",
+    "simeji/winresizer",
+    {
+        "akinsho/toggleterm.nvim",
+        lazy = true,
+        config = function()
+            require("toggleterm").setup({
+                open_mapping = [[<C-\>]],
+                size = 20,
+                hide_numbers = true,
+                direction = "float",
+                close_on_exit = true,
+            })
+        end
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        lazy = true,
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-nvim-lua",
+            "L3MON4D3/LuaSnip",
+        },
+        config = function()
+            require("config/cmp")
+        end
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        lazy = true,
+        config = true,
+    },
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            "jose-elias-alvarez/null-ls.nvim",
+            "MunifTanjim/prettier.nvim",
+        },
+        config = function()
+            require("lsp")
+        end
+    },
+    {
+        "simrat39/rust-tools.nvim",
+        lazy = true,
+        config = true,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        lazy = true,
+        config = true,
+    },
+    {
+        "j-hui/fidget.nvim",
+        lazy = false,
+        config = true,
+    },
+    {
+        "lewis6991/impatient.nvim",
+    }
+})
