64fdb528cf534c0992e220618b646fd300929ac6
ryu <ryu000407@gmail.com>
2023-07-08 02:57:11 +0900

    update: undo for wezterm

--- packages/hammerspoon/.hammerspoon/init.lua
+++ packages/hammerspoon/.hammerspoon/init.lua
@@ -1,10 +1,10 @@
 hs.hotkey.bind({ "ctrl" }, "g", function()
-	local wezterm = hs.application.get("Rio")
+	local wezterm = hs.application.get("WezTerm")
 	if wezterm == nil then
-		hs.application.launchOrFocus("/Applications/Rio.app")
+		hs.application.launchOrFocus("/Applications/WezTerm.app")
 	elseif wezterm:isFrontmost() then
 		wezterm:hide()
 	else
-		hs.application.launchOrFocus("/Applications/Rio.app")
+		hs.application.launchOrFocus("/Applications/WezTerm.app")
 	end
 end)
