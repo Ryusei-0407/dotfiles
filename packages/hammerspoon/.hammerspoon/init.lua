afa1df381050455ef919985e361944d09d2b9bff
ryu <ryu000407@gmail.com>
2023-07-08 02:40:45 +0900

    update: change wezterm to rio

--- packages/hammerspoon/.hammerspoon/init.lua
+++ packages/hammerspoon/.hammerspoon/init.lua
@@ -1,10 +1,10 @@
 hs.hotkey.bind({ "ctrl" }, "g", function()
-	local wezterm = hs.application.get("WezTerm")
+	local wezterm = hs.application.get("Rio")
 	if wezterm == nil then
-		hs.application.launchOrFocus("/Applications/WezTerm.app")
+		hs.application.launchOrFocus("/Applications/Rio.app")
 	elseif wezterm:isFrontmost() then
 		wezterm:hide()
 	else
-		hs.application.launchOrFocus("/Applications/WezTerm.app")
+		hs.application.launchOrFocus("/Applications/Rio.app")
 	end
 end)
