f54dd022205e60af3fb7a010374ac0be61febe66
ryu <ryu000407@gmail.com>
2023-09-25 23:07:06 +0900

    update(sheldon): create sheldon config

---
+++ packages/config/sheldon/sheldon.zsh
@@ -0,0 +1,5 @@
+source "/Users/ryu/.local/share/sheldon/repos/github.com/romkatv/powerlevel10k/powerlevel10k.zsh-theme"
+source "/Users/ryu/.local/share/sheldon/repos/github.com/romkatv/zsh-defer/zsh-defer.plugin.zsh"
+zsh-defer source "/Users/ryu/.local/share/sheldon/repos/github.com/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
+source "/Users/ryu/.local/share/sheldon/repos/github.com/zdharma-continuum/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh"
+source "/Users/ryu/.local/share/sheldon/repos/github.com/zdharma-continuum/history-search-multi-word/history-search-multi-word.plugin.zsh"
