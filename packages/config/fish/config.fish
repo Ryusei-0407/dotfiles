2839ff248ed39b597a7eb715b988a0a0608fff1f
ryu <ryu000407@gmail.com>
2026-04-10 02:15:13 +0900

    update(fish): change color theme

--- packages/config/fish/config.fish
+++ packages/config/fish/config.fish
@@ -5,6 +5,8 @@ fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/sbin
 fish_add_path /opt/homebrew/opt/openssl@1.1/bin
 
+fish_config theme choose token
+
 set -gx EDITOR nvim
 set -gx VISUAL nvim
 
@@ -18,12 +20,14 @@ alias vim nvim
 alias j z
 alias a "cd .."
 alias ls "eza --git --icons"
+alias la "eza --git --icons -a"
 alias ll "eza -l --git --icons --no-user"
 alias tree "eza --git --icons --git-ignore --tree"
 alias cp "cp -p"
 alias mkdir "mkdir -p"
 
 abbr -a md treemd
+abbr -a jl jless
 abbr -a cut choose
 abbr -a lg lazygit
 abbr -a hex hexyl
@@ -32,7 +36,6 @@ abbr -a nsl nslookup
 abbr -a pbc pbcopy
 abbr -a pbp pbpaste
 abbr -a watch viddy
-abbr -a wget pget
 abbr -a m make
 abbr -a dps "docker ps"
 abbr -a dimg "docker images"
@@ -40,6 +43,7 @@ abbr -a dup "docker compose up -d"
 abbr -a ddown "docker compose down"
 abbr -a dprune "yes | docker volume prune && yes | docker sytem prune --all"
 abbr -a glog "git log --oneline --graph"
+abbr -a site2skill "uvx --from git+https://github.com/laiso/site2skill site2skill"
 
 fish_default_key_bindings
 
@@ -53,3 +57,7 @@ set -U fish_greeting ""
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
